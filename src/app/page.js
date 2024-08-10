560bf003f1060b9fc09f8ae28232cefc63315d80
Fozle Rabbi <fozlerabbi9790@gmail.com>
2024-08-10 21:56:54 +0600

    Breaking the Limits of Static Sites with Next

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,22 @@
+import Image from "next/image";
+
 export const metadata = {
   title: "Home Page",
   description: "This is home page"
 }
 
 const HomePage = async () => {
-  const res = await fetch("http://localhost:5000/shoes");
+  const res = await fetch("http://localhost:5000/shoes", {
+    // cache: "force-cache"
+    next : {
+      revalidate : 30
+    }
+  });
   const shoes = await res.json();
 
   return (
     <div>
-      <h2 className='text-3xl text-center'>This is home page</h2>
+      <h2 className='text-3xl text-center'>This is home page 222</h2>
 
       <div className="grid grid-cols-3 gap-14 px-10">
         {
@@ -17,20 +24,23 @@ const HomePage = async () => {
             className="card bg-base-100  shadow-xl"
             key={shoe.id} >
             <figure>
-              <img
+              <Image
+                width={500}
+                height={500}
                 src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-                alt="Shoes" />
+                alt="Shoes"
+              />
+
             </figure>
             <div className="card-body">
               <h2 className="card-title">
                 {shoe.title}
-                <div className="badge badge-secondary">NEW</div>
+                <div className="badge badge-secondary">{shoe.price} <span className="italic">$</span></div>
               </h2>
               <p>{shoe.description}</p>
-              <p>Price : {shoe.price} <span className="italic">$</span></p>
               <div className="card-actions justify-end">
-                <div className="badge badge-outline">Fashion</div>
-                <div className="badge badge-outline">Products</div>
+                <button className="btn btn-sm w-[100px] btn-outline btn-primary">By Now</button>
+                <button className="btn btn-sm w-[100px] btn-outline btn-secondary">Details</button>
               </div>
             </div>
           </div>
