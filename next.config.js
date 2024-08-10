f03b5c59d0af81a104f727256e678b27bb544c4c
Fozle Rabbi <fozlerabbi9790@gmail.com>
2024-08-10 16:03:42 +0600

    dynamic image optimizarion and text oprimization

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['png.pngtree.com', 'onebighappyphoto.com'],
+        // domains: ['png.pngtree.com', 'onebighappyphoto.com'], //======>>> Manuyal sistem
+        remotePatterns: [   //========>>> dynamic
+            {
+                protocol: 'https',
+                hostname: '**', // universel **  এর ফলে যত প্রকার hostnmae আছে সব support করবে 
+            },
+        ],
     },
 };
 
--- src/app/gallery/page.js
+++ src/app/gallery/page.js
@@ -1,5 +1,8 @@
+/* eslint-disable @next/next/no-img-element */
+/* eslint-disable jsx-a11y/alt-text */
 import Image from 'next/image';
 import React from 'react';
+import baby from '../../assets/newbaby.jpg'
 
 const GalleryPage = () => {
     return (
@@ -7,10 +10,25 @@ const GalleryPage = () => {
             <h2 className='text-center text-3xl'>Image oprimizations</h2>
             <h2 className='text-center text-2xl'>Regular Image Tag</h2>
 
-            <img className='mx-auto' width={650} height={500} src="https://onebighappyphoto.com/wp-content/uploads/Newborn-Photos-full-of-color-6517-One-Big-Happy-Photo.jpg" alt="next image" />
+            <img className='mx-auto'
+             width={650} 
+             height={500} 
+            //  src={baby} ======>>> normal img tag এ dynamic image support করবে না
+             src="https://onebighappyphoto.com/wp-content/uploads/Newborn-Photos-full-of-color-6517-One-Big-Happy-Photo.jpg" alt="next image" />
 
             <h2 className='text-center text-2xl mt-4'>Next Component Image Tag</h2>
-            <Image width={650} height={500} className='mx-auto' src='https://onebighappyphoto.com/wp-content/uploads/Newborn-Photos-full-of-color-6517-One-Big-Happy-Photo.jpg' />
+            <Image
+                width={650}
+                height={500}
+                // fill  =============>>> fill use করেলে hight width আর কাজ করবে না 
+                className='mx-auto' src='https://onebighappyphoto.com/wp-content/uploads/Newborn-Photos-full-of-color-6517-One-Big-Happy-Photo.jpg' />
+
+            <h2 className='text-center text-2xl mt-4'>Local Image Tag</h2>
+            <Image
+                width={650}
+                height={500}
+                // fill  =============>>> fill use করেলে hight width আর কাজ করবে না 
+                className='mx-auto' src={baby} />
 
 
         </div>
